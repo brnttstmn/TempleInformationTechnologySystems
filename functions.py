0ff5000c5fd79a78d4a4dbd90a14375eab65f90d
dcnielsen <dcnielsen90@gmail.com>
2016-12-07 15:40:17 -0500

    Revenue/Product Function
    Added revenue per product function.

---
+++ functions.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+def prodsale(season):
+	sItem={}
+	qItem={}
+	show=20
+	count=0
+	
+	for i in range(len(season.PROD_NBR)):
+		prod = (season['PROD_NBR']).iloc[i]
+		qty = (season['SLS_QTY']).iloc[i]
+		pos = (season['POS_AMT']).iloc[i]
+		if prod in sItem:
+			qItem[prod] = qItem[prod] + qty
+			sItem[prod] = sItem[prod] + pos
+		else:
+			qItem[prod] = qty
+			sItem[prod] = pos
+			
+	print("{:25} {:>6} {:>6}".format("Product Number","Gross Income","Qty"))
+	for i in sorted(sItem, key=sItem.get, reverse=True):
+		value = sItem[i]
+		quant = qItem[i]
+		print("{:25} {:6.2f} {:>10}".format(i, value, quant))
+		count=count+1
+		if(count>show):
+			break
